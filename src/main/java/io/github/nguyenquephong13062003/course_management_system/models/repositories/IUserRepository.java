5a075520cf29cb33d4e6349a87d42ae0f2fe63ba
Thomas Phong <ino.thomas@outlook.com>
2026-08-05 17:43:51 +0700

    build: create first user repository
    - Extends JpaRepository<User,Long>
    - Add Function: Optional<User> findByUsername(String username)

---
+++ src/main/java/io/github/nguyenquephong13062003/course_management_system/models/repositories/IUserRepository.java
@@ -0,0 +1,21 @@
+package io.github.nguyenquephong13062003.course_management_system.models.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.github.nguyenquephong13062003.course_management_system.models.entities.User;
+
+/**
+ * User Repository
+ */
+public interface IUserRepository extends JpaRepository<User,Long> {
+
+    /**
+     * Find a user by their username.
+     *
+     * @param username the username of the user to find
+     * @return an Optional containing the found User, or empty if not found
+     */
+    Optional<User> findByUsername(String username);
+}
