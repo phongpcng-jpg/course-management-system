1d806ed37987737f4826b69463e9e8fbe1babafa
Thomas Phong <ino.thomas@outlook.com>
2026-08-14 14:46:49 +0700

    build: authController version 3
    - Add get /me

--- src/main/java/io/github/nguyenquephong13062003/course_management_system/controllers/AuthController.java
+++ src/main/java/io/github/nguyenquephong13062003/course_management_system/controllers/AuthController.java
@@ -1,6 +1,7 @@
 package io.github.nguyenquephong13062003.course_management_system.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.github.nguyenquephong13062003.course_management_system.models.dtos.requests.LoginRequest;
 import io.github.nguyenquephong13062003.course_management_system.models.dtos.responses.LoginResponse;
+import io.github.nguyenquephong13062003.course_management_system.models.dtos.responses.UserResponse;
 import io.github.nguyenquephong13062003.course_management_system.models.dtos.responses.VerifyResponse;
 import io.github.nguyenquephong13062003.course_management_system.models.dtos.wrappers.ApiResponse;
 import io.github.nguyenquephong13062003.course_management_system.models.services.IAuthService;
@@ -56,6 +58,20 @@ public class AuthController {
             200,
             "Token is valid",
             authService.verifyToken()
+        ));
+    }
+
+    /**
+     * Retrieves the profile of the currently authenticated user.
+     *
+     * @return A ResponseEntity containing the current user's profile wrapped in an ApiResponse.
+     */
+    @GetMapping("/me")
+    public ResponseEntity<ApiResponse<UserResponse>> me() {
+        return ResponseEntity.status(200).body(ApiResponse.<UserResponse>success(
+            200,
+            "User profile retrieved successfully",
+            authService.getCurrentUser()
         ));
     }
 
