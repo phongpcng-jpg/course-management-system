de1959e9b43a6a5d96c91eb2649884d161cfe4a5
Nguyen Que Phong <nguyenquephong13062003@gmail.com>
2026-08-17 16:37:34 +0700

    build: create EnrollmentStatus enum

---
+++ src/main/java/io/github/nguyenquephong13062003/course_management_system/models/constants/EnrollmentStatus.java
@@ -0,0 +1,24 @@
+package io.github.nguyenquephong13062003.course_management_system.models.constants;
+
+/**
+ * Enumeration representing the possible enrollment statuses for a course.
+ * This enum defines the different states that a user's enrollment in a course can have.
+ */
+public enum EnrollmentStatus {
+    
+    /**
+     * Indicates that the user is currently enrolled in the course.
+     */
+    ENROLLED,
+
+    /**
+     * Indicates that the user has completed the course.
+     */
+    COMPLETED,
+
+    /**
+     * Indicates that the user has dropped the course.
+     */
+    DROPPED
+    
+}
