fabfc078dbe5f4a2b76c2e7e6c3ff676502d6169
Nguyen Que Phong <nguyenquephong13062003@gmail.com>
2026-08-17 14:49:00 +0700

    build: update ILessonRepository
    - add findByCourse_IdAndIsPublishedTrueOrderByOrderIndexAsc

--- src/main/java/io/github/nguyenquephong13062003/course_management_system/models/repositories/ILessonRepository.java
+++ src/main/java/io/github/nguyenquephong13062003/course_management_system/models/repositories/ILessonRepository.java
@@ -3,9 +3,22 @@ package io.github.nguyenquephong13062003.course_management_system.models.reposit
 import io.github.nguyenquephong13062003.course_management_system.models.entities.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Repository interface for managing Lesson entities.
  * This interface extends JpaRepository, providing CRUD operations and additional query methods for Lesson entities.
  */
 public interface ILessonRepository extends JpaRepository<Lesson, Long> {
+
+    /**
+     * Retrieves a list of published lessons for a specific course, ordered by their order index in ascending order.
+     *
+     * @param courseId The ID of the course for which to retrieve published lessons.
+     * @return A list of published Lesson entities associated with the specified course, ordered by order index.
+     */
+    List<Lesson> findByCourse_IdAndIsPublishedTrueOrderByOrderIndexAsc(
+            Long courseId
+    );
+
 }
